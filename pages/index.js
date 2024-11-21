8fcb1d225e4b67a3fc97941c489930656dd02442
Chowdhury Isfatul Karim <sftsiam@gmail.com>
2024-11-21 15:48:25 +0600

    mobile bug fix

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const inter = Inter({
 export default function Home() {
   return (
     <div className={`flex justify-center bg-[#FAFEFC] ${inter.className}`}>
-      <div className="flex flex-col w-full max-w-screen-2xl px-4 lg:px-2">
+      <div className="flex flex-col w-full max-w-screen-2xl overflow-hidden px-4 lg:px-2">
         <div className=""><Navbar /></div>
         <div className="py-11 w-full"><Hero/></div>
         <div><WhyChose/></div>
